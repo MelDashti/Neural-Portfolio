e32b6f5d15600827488da8bd3dab254ca0f497a7
Meelad Dashti <72992585+MelDashti@users.noreply.github.com>
2025-12-27 10:28:02 +0100

    configure base path for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://meldashti.github.io',
+  base: '/Neural-Portfolio',
   output: 'static',
   build: {
     assets: 'assets'
